088bcba54f4ccf1b6414bcf4111386ca17629247
yalin:turtle [ubuntu-22.04] <yalindemir@outlook.de>
2025-12-24 13:33:17 +0100

    moving the robot up

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -47,7 +47,8 @@ def generate_launch_description():
         arguments=[
             '--ros-args',
             '-p',
-            f'config_file:={bridge_params}',]
+            f'config_file:={bridge_params}',
+            ]
     )
 
     gz_proc = ExecuteProcess(
